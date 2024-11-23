3abdcc1e1259c9a742e4df9fd2377ea07cf2045e
Hemanvitha Pullela <hemanvithapullela@gmail.com>
2024-11-24 00:04:27 +0530

    linked it with backend

--- amazon-smb-dashboard/vite.config.js
+++ amazon-smb-dashboard/vite.config.js
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Specifies the output directory for production builds
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://keen-unicorn-487a0d.netlify.app/.netlify/functions/api", // Proxy to your Netlify function endpoint
+        changeOrigin: true,
+        rewrite: (path) => {
+          const url = new URL(path, "https://keen-unicorn-487a0d.netlify.app/.netlify/functions/api")
+          return `${url.pathname}`; // Strips `/api` and keeps the original path
+        },
+      },
+    },
   },
 })
